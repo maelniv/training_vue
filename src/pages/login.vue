8331b1d3f051ef1cf6b851d70e95740861d946d2
mael <mael.nivel@gmail.com>
2024-03-01 16:33:53 +0100

    t

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -51,31 +51,58 @@ watch([email, password], () => {
 </script>
 
 <template>
-    <div>
-        <v-sheet class="mx-auto" width="300">
-            <v-form @submit.prevent="handleSubmit" error-messages="test">
-                <div class="text-subtitle-1 text-medium-emphasis">Username</div>
-                <v-text-field name="email" v-model="email" density="compact" placeholder="Email address"
-                    prepend-inner-icon="mdi-email-outline" variant="outlined" autocomplete="username"
-                    :error-messages="errors.email"></v-text-field>
+    <div class="split-container">
+        <div class="left-section">
+            <img src="@/assets/logo-wiiisdom-ops.png" alt="Wiiisdom logo">
+        </div>
+        <div class="right-section">
+            <v-sheet class="mx-auto" width="300">
+                <v-form @submit.prevent="handleSubmit" error-messages="test">
+                    <div class="text-subtitle-1 text-medium-emphasis">Username</div>
+                    <v-text-field name="email" v-model="email" density="compact" placeholder="Email address"
+                        prepend-inner-icon="mdi-email-outline" variant="outlined" autocomplete="username"
+                        :error-messages="errors.email"></v-text-field>
 
-                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                    Password
-                </div>
-                <v-text-field :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'" :type="isPasswordVisible ? 'text' : 'password'"
-                    density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline"
-                    variant="outlined" v-model="password" autocomplete="current-password"
-                    @click:append-inner="isPasswordVisible = !isPasswordVisible" :error-messages="errors.password">
-                </v-text-field>
+                    <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                        Password
+                    </div>
+                    <v-text-field :append-inner-icon="isPasswordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="isPasswordVisible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+                        prepend-inner-icon="mdi-lock-outline" variant="outlined" v-model="password"
+                        autocomplete="current-password" @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                        :error-messages="errors.password">
+                    </v-text-field>
 
+                    <v-btn block class="mb-8" color="wiiisdom_orange" size="large" variant="tonal" type="submit">
+                        Log In
+                    </v-btn>
 
-                <v-btn block class="mb-8" color="wiiisdom_orange" size="large" variant="tonal" type="submit">
-                    Log In
-                </v-btn>
-
-                <v-alert v-if="errorMessage != ''" color="error" icon="$error" title="Erreur" :text="errorMessage">
-                </v-alert>
-            </v-form>
-        </v-sheet>
+                    <v-alert v-if="errorMessage != ''" color="error" icon="$error" title="Erreur" :text="errorMessage">
+                    </v-alert>
+                </v-form>
+            </v-sheet>
+        </div>
     </div>
 </template>
+  
+<style scoped>
+.split-container {
+    display: flex;
+    height: 100vh;
+}
+
+.left-section {
+    flex: 1;
+    background: linear-gradient(to bottom, #0393f0, #1f1d74);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.right-section {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
